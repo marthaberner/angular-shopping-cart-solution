b38cb93995ea53ebf494ea1d0d6f22e2adcabf8a
marthaberner <marthaberner.mb@gmail.com>
2016-08-15 22:42:41 -0600

    user can sort teas by price

--- js/controllers/teas_controller.js
+++ js/controllers/teas_controller.js
@@ -1,6 +1,14 @@
 app.controller('TeasController', [ '$http', 'priceFilter', function ($http, priceFilter) {
   var _this = this;
+  _this.sortTitle = '';
+
   $http.get('/data/tea.json').success(function (teas) {
     _this.all = teas;
   })
+
+  _this.setSort = function (choice) {
+    _this.sortTitle = choice;
+    choice = choice === 'highest' ? "-price" : "price";
+    _this.sortBy = choice;
+  }
 }])
